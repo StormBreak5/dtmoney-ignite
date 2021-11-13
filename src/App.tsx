fbbdb2a183a1e7d46600ac7dc02daf2ed2e25794
Itzac <itzacalbertin@gmail.com>
2021-11-13 18:08:37 -0300

    added modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNTModalOpen, setIsNTModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNTModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNTModalOpen(false);
+    }
+    
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard/>
+      <Modal isOpen={isNTModalOpen} onRequestClose={handleCloseNewTransactionModal}>
+          <h2>Cadastrar info</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
